5d65895ff7c4d27af94dddeee7cd3df2958834f8
Faffo96 <fabioscar96@gmail.com>
2024-06-18 11:50:05 +0200

    first commit

---
+++ hamburgeria/backend/hamburgeria_backend/src/test/java/com/koyeb/hamburgeria_backend/HamburgeriaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koyeb.hamburgeria_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamburgeriaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
