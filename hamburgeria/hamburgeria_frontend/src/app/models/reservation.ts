cb76a4684424a2d9674c9ba12a862b832b5cf36b
Faffo96 <fabioscar96@gmail.com>
2024-06-22 17:29:19 +0200

    Components and Interfaces creation

--- hamburgeria/hamburgeria_frontend/src/app/models/reservation.ts
+++ hamburgeria/hamburgeria_frontend/src/app/models/reservation.ts
@@ -1,2 +1,13 @@
+import { Cart } from "./cart";
+import { DiningTable } from "./dining-table";
+import { User } from "./user";
+
 export interface Reservation {
-}
+    id: number;
+    creationDate: string;
+    bookedDate: string;
+    diningTable: DiningTable;
+    user: User;
+    participants: number;
+    cart: Cart;
+  }
--- hamburgeria/hamburgeria_frontend/src/app/models/shift.ts
+++ hamburgeria/hamburgeria_frontend/src/app/models/shift.ts
@@ -1,2 +1,8 @@
+import { Employee } from "./employee";
+
 export interface Shift {
-}
+    id: number;
+    employee: Employee;
+    startDate: string; 
+    endDate: string;
+  }
